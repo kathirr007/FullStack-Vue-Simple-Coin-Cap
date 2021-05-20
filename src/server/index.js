42e62a8c0b11f4f467dbea2793c85295e5962291
Kathiravan Kumarasamy <kathiravan.kumarasamy@adelaide.edu.au>
2021-05-21 01:28:43 +0530

    Update server for heroku

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const serveStatic = require('serve-static')
 const axios = require('axios')
 const path = require('path')
 const history = require('connect-history-api-fallback')
@@ -12,14 +13,25 @@ const port = process.env.PORT || 3000
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
 
 if (process.env.NODE_ENV == 'production') {
-  app.use(staticFileMiddleware)
+  /* app.use(staticFileMiddleware)
   app.use(
     history({
       disableDotRule: true,
       verbose: true
     })
   )
-  app.use(staticFileMiddleware)
+  app.use(staticFileMiddleware) */
+
+  app.use('/', serveStatic(path.join(__dirname, '/dist')))
+  app.use(
+    history({
+      disableDotRule: true,
+      verbose: true
+    })
+  )
+  app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+  })
 }
 
 app.get('/api/coins', function (req, res) {
